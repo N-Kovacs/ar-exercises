3f6b94342869fa0a20eccb5b1080dd46e91bd532
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-11-14 21:44:33 +0000

    exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "first", last_name: "last", hourly_rate: 70)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Fake", last_name: "Bame", hourly_rate: 60)
+@store1.employees.create(first_name: "Name", last_name: "Also", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,30 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# class Store < ApplicationRecord
+#   validate :carry
+#   def carry
+#     if mens_apparel == false && womens_apparel == false
+#       errors.add("Has to carry apparel")
+#     end
+#   end
+# end
+
+
+
+class Store < ActiveRecord::Base
+  validates :name, length: {minimum: 3} 
+  validates :annual_revenue, numericality: {only_integer: true}
+end
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :store_id, presence: true
+  validates :hourly_rate, numericality: {:only_integer => true, :less_than => 200, :greater_than => 40}
+end
+
+puts "Enter store name"
+sName = gets.chomp
+store7 = Store.create(name: sName)
+puts store7.errors.full_messages
